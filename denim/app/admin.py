9799aab88856c436a35bc473a6ad7f37c8941124
MITESH PATEL <mitesh64837@gmail.com>
2025-06-05 22:36:47 +0530

    added models

--- denim/app/admin.py
+++ denim/app/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Product, ProductImage
 
 # Register your models here.
+
+class ProductImageInline(admin.TabularInline):
+    model = ProductImage
+    extra = 1  
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display=[
+        "name",
+        "category",
+        "description",
+        "price",
+        "stock"
+        ]
+    inlines = [ProductImageInline]
+
+admin.site.register(Product,ProductAdmin)
--- denim/app/models.py
+++ denim/app/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    category = models.CharField(max_length=50, choices=[
+        ('Jacket', 'Jacket'),
+        ('Jeans', 'Jeans')
+    ])
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.name 
+    
+class ProductImage(models.Model):
+    Product = models.ForeignKey(Product, related_name="images", on_delete=models.CASCADE)
+    image = models.ImageField(upload_to="projects/images/")
+
+    def __str__(self):
+        return f"Image for {self.Product.name}"
